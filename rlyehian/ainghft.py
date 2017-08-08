7aff3f5a3f13a0151329db5716064f2328282678
Jimmy Le <jldevops@users.noreply.github.com>
2017-08-07 22:49:48 -0400

    Updated dictionary.py for word association collection.

---
+++ rlyehian/ainghft.py
@@ -0,0 +1,9 @@
+from dictionary import api_call_definitions
+
+read_path = '/Users/destinesavior/Desktop/Hidden-Desktop/Project/rlyehian/rlyehian/rlyehian/kadishtuor/'
+read_filename = 'words-list.csv'
+write_filename = 'word.csv'
+
+print api_call_definitions("walk")
+
+# create_word_file(read_path, read_filename, write_filename)
